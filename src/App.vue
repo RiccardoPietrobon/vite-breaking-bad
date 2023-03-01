5c8784fc6f4fae59f21e1f9eaadea8b3f0963f20
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-03-01 16:24:16 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,25 @@
-!vue
+<script>
+import JugiMain from "./components/JugiMain.vue";
+import NavBar from "./components/NavBar.vue";
+
+
+export default {
+  data() {
+    return {
+      title: "Hello",
+    };
+  },
+
+  components: { JugiMain, NavBar },
+};
+</script>
+
+<template>
+  <NavBar />
+  <JugiMain />
+</template>
+
+<style lang="scss" scoped>
+@use "./components/scss/partials/style.scss" as *;
+</style>
 
---
+++ src/components/BoxCard.vue
@@ -0,0 +1,12 @@
+<script>
+
+
+export default {
+};
+</script>
+
+<template></template>
+
+<style lang="scss" scoped>
+@use "../components/scss/partials/style.scss" as *;
+</style>
